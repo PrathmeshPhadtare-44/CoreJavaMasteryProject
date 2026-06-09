3a48d375f696e02c9c381486b78aa717e6d1be9d
PrathmeshPhadtare-44 <prathmeshphadtare1@gmail.com>
2026-06-10 00:34:31 +0530

    solve String Basic Operations.

---
+++ src/com/prathmesh/corejavamastery/string/StringBasicOperations.java
@@ -0,0 +1,37 @@
+package com.prathmesh.corejavamastery.string;
+//Easy
+//Question 1
+//String text = "Programming";
+//Print:
+//Length
+//First character
+//Last character
+//Uppercase version
+//Lowercase version
+//Whether it contains "gram"
+//Expected Output Example
+//Length : 11
+//First Character : P
+//Last Character : g
+//Uppercase : PROGRAMMING
+//Lowercase : programming
+//Contains gram : true
+public class StringBasicOperations {
+    public static void main(String[] args) {
+        String text = "Programming";
+        //Length
+        System.out.println("Length : "+text.length());
+        //First character
+        System.out.println("First Character :"+text.charAt(0));
+        // Last character
+        System.out.println("First Character :"+text.charAt(text.length() - 1));
+
+        //Uppercase version
+        System.out.println("UpperCase :"+text.toUpperCase());
+        //Lowercase version
+        System.out.println("LowerCase :"+text.toLowerCase());
+
+        //Whether it contains "gram"
+        System.out.println("Contains gram :"+text.contains("gram"));
+    }
+}
