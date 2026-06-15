c9930e87a7618733d5d6dc2f6f637a517db03802
PrathmeshPhadtare-44 <prathmeshphadtare1@gmail.com>
2026-06-15 22:40:35 +0530

    Solve OOPS student information  questions.

--- src/com/prathmesh/corejavamastery/opps/StudentInformation.java
+++ src/com/prathmesh/corejavamastery/opps/StudentInformation.java
@@ -15,7 +15,54 @@ package com.prathmesh.corejavamastery.opps;
  * 4. Display details.
  * 5. Display Pass/Fail.
  */
+
 public class StudentInformation {
+    public static void main(String[] args) {
+        Student student =  new Student(
+                "Prathmesh" ,
+                21 ,
+                75.32);
+        System.out.println(student);
+        if(student.isPass()){
+            System.out.println("Student is Pass.");
+        }
+        else {
+            System.out.println("Student is failed.");
+        }
+    }
+
+}
+class Student{
+    private  String name;
+    private  int age;
+    private  double mark;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", mark=" + mark +
+                '}';
+    }
 
 
+
+
+    public Student(String name , int age , double mark ){
+        if (age < 0){
+            throw new IllegalArgumentException("Age cannot be negative ");
+        }
+        if(mark < 0 || mark > 100){
+            throw new IllegalArgumentException("Invalid marks");
+        }
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+    }
+
+
+    public boolean isPass(){
+        return  mark >= 35;
+    }
 }
